7f1a34f9ee22f60aaf4c7decd687dae9a26d6c65
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-22 21:23:16 -0400

    added affirmation list and init state

--- lib/pages/affirmations_page.dart
+++ lib/pages/affirmations_page.dart
@@ -10,3 +10,20 @@ class AffirmationsPage extends StatefulWidget {
 class _AffirmationsPageState extends State<AffirmationsPage> {
   static const _kAffirmIdx = 'affirmation_index';
   static const _kAffirmDate = 'affirmation_date';
+  
+  final _quotes = const [
+    'I inhale calm and exhale stress.',
+    'I am present. I am grounded.',
+    'Every breath resets my mind.',
+    'I choose peace over worry.',
+    'Small steps today create big change.',
+    'I treat myself with kindness.',
+  ];
+
+  int _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadOrRotate();
+  }
