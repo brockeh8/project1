12e51420758673fb36fb82ff7f48c1caaf3d7bdb
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-22 21:19:57 -0400

    declared widget

--- lib/pages/affirmations_page.dart
+++ lib/pages/affirmations_page.dart
@@ -1,2 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+class AffirmationsPage extends StatefulWidget {
+  const AffirmationsPage({super.key});
+  @override
+  State<AffirmationsPage> createState() => _AffirmationsPageState();
+}
