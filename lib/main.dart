63b354abd96ae53074c56955fa742cdb961c503f
brockeh8 <bfreiberger1@student.gsu.edu>
2025-10-24 15:37:59 -0400

    Trying to start the timer with the breathing widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
-// import 'pages/breathing_page.dart';
-// import 'pages/meditation_page.dart';
+import 'pages/breathing_page.dart';
+import 'pages/meditation_page.dart';
 import 'pages/mood_page.dart';
-// import 'pages/journal_page.dart';
-// import 'pages/affirmations_page.dart';
+import 'pages/journal_page.dart';
+import 'pages/affirmations_page.dart';
 
 void main() => runApp(const MindfulnessApp());
 
@@ -24,13 +24,13 @@ const MindfulnessApp({super.key});
       ),
       home :const HomePage(),
       routes: {
-      //   //routes blank till update shooting for later today
-      //   '/': (_) => const HomePage(),
-      //   '/breathing': (_) => const BreathingPage(),
-      //   '/meditation': (_) => const MeditationPage(),
+        //routes blank till update shooting for later today
+        '/': (_) => const HomePage(),
+        '/breathing': (_) => const BreathingPage(),
+        '/meditation': (_) => const MeditationPage(),
         '/mood': (_) => const MoodPage(),
-      //   '/journal': (_) => const JournalPage(),
-      //   '/affirmations': (_) => const AffirmationsPage(),
+        '/journal': (_) => const JournalPage(),
+        '/affirmations': (_) => const AffirmationsPage(),
       },
     );
   }
--- lib/pages/breathing_page.dart
+++ lib/pages/breathing_page.dart
@@ -2,11 +2,28 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class BreathingPage extends StatefulWidget {
-  const BreathingPage({super.key});
-  @override
-  State<BreathingPage> createState() => _BreathingPageState();
+    const BreathingPage({super.key});
+    @override
+    State<BreathingPage> createState() => _BreathingPageState();
 }
+class _BreathingPageState extends State<BreathingPage> {
+    bool _running = false;
+    double _size = 120;
+    Timer? _timer;
+    int _phase = 0; //change depending on timing
 
+    void _start() {
+        _running = true;
+        _phase = 0;
+        _cycle();
+    }
+
+    void _cycle() {
+        //rest of timer
+    }
+
+    //research how to do animaiton linked to timer
+    
 //breathing
 
 //animation pro
