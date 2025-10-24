d50d3b0f825c3302c028d6f8965427f24ec0a552
brockeh8 <bfreiberger1@student.gsu.edu>
2025-10-24 15:18:27 -0400

    Starting the breathing page with the animation

---
+++ lib/pages/breathing_page.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class BreathingPage extends StatefulWidget {
+  const BreathingPage({super.key});
+  @override
+  State<BreathingPage> createState() => _BreathingPageState();
+}
+
+//breathing
+
+//animation pro
+
+//final widget creation + text
+
