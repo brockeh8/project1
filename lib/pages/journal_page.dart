e11d7c30609d4eeb5e1b9ada361f539b8f92f5a5
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-23 22:56:01 -0400

    added state class and variables

--- lib/pages/journal_page.dart
+++ lib/pages/journal_page.dart
@@ -7,3 +7,14 @@ class JournalPage extends StatefulWidget {
   @override
   State<JournalPage> createState() => _JournalPageState();
 }
+
+class _JournalPageState extends State<JournalPage> {
+  static const _kJournal = 'journal_entries'; // JSON list of {date, text}
+  final _controller = TextEditingController();
+  List<Map<String, String>> _entries = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
