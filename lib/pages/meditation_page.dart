2285783ba37bde5ee880a9073760de212908700f
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-24 23:35:21 -0400

    decalaration of state variables and timer

--- lib/pages/meditation_page.dart
+++ lib/pages/meditation_page.dart
@@ -6,3 +6,9 @@ class MeditationPage extends StatefulWidget {
   @override
   State<MeditationPage> createState() => _MeditationPageState();
 }
+
+class _MeditationPageState extends State<MeditationPage> {
+  int _seconds = 60; // default 1 min
+  int _left = 60;
+  Timer? _timer;
+  bool get _running => _timer?.isActive ?? false;
