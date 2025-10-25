4eec0cd11fdebab74fdf94ff3dc5315c860c3fa4
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-24 23:33:54 -0400

    imports and declaration of widget

---
+++ lib/pages/meditation_page.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class MeditationPage extends StatefulWidget {
+  const MeditationPage({super.key});
+  @override
+  State<MeditationPage> createState() => _MeditationPageState();
+}
