ed97453d89d7715cddd788c46e3fd10240ae84a7
JJPatel03 <jpatel194@student.gsu.edu>
2025-10-24 23:36:49 -0400

    control methods for the timer

--- lib/pages/meditation_page.dart
+++ lib/pages/meditation_page.dart
@@ -12,3 +12,30 @@ class _MeditationPageState extends State<MeditationPage> {
   int _left = 60;
   Timer? _timer;
   bool get _running => _timer?.isActive ?? false;
+
+  void _start() {
+    _left = _seconds;
+    _timer?.cancel();
+    _timer = Timer.periodic(const Duration(seconds: 1), (t) {
+      if (_left <= 1) {
+        t.cancel();
+        setState(() => _left = 0);
+      } else {
+        setState(() => _left -= 1);
+      }
+    });
+    setState(() {});
+  }
+
+  void _stop() {
+    _timer?.cancel();
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  String _fmt(int s) => '${(s ~/ 60).toString().padLeft(2, '0')}:${(s % 60).toString().padLeft(2, '0')}';
